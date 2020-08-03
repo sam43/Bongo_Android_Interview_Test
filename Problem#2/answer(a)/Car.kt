a75b044443983399cad0c57cf9231e3328f2be7b
A S M Sayem <scode43@gmail.com>
2020-08-03 22:25:14 +0600

    Created Car class

---
+++ Problem#2/answer(a)/Car.kt
@@ -0,0 +1,19 @@
+package com.sam43.bongovideoplayer.builderpattern.answerA
+
+class Car(
+    private val num_of_wheels: Int,
+    private val num_of_passengers: Int,
+    private val has_gas: Boolean
+) : Vehicle {
+    override fun set_num_of_wheels(): Int = num_of_wheels
+
+    override fun set_num_of_passengers(): Int = num_of_passengers
+
+    override fun has_gas(): Boolean = has_gas
+
+    fun drive() {
+        println("Car is driving...")
+    }
+}
+
+
