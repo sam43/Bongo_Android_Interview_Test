3d166fb372b33f37b3ae1f6fc282fabb298e4742
sayem <asm.sayem@brightbrains.ae>
2020-08-04 20:11:47 +0600

    Update - Remove extra files
    Removing extra files and renaming folders

--- Problem#2/DesignPatterProblem/app/src/main/java/com/sam43/designpatterproblem/builderpattern/answerA/Vehicle.kt
+++
@@ -1,7 +0,0 @@
-package com.sam43.designpatterproblem.builderpattern.answerA
-
-interface Vehicle {
-    fun set_num_of_wheels(): Int
-    fun set_num_of_passengers(): Int
-    fun has_gas(): Boolean
-}
